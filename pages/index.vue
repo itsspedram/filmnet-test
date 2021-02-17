4356d08df5596933104cc37b8e97e084c662fc5d
pedram <pedmsv@gmail.com>
2021-02-17 23:58:17 +0330

    craet edit and add users functions

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,133 @@
 <template>
   <div>
     <div class="container">
-      <div v-for="(user, index) in users" :key="user.id" class="row">
-        <div class="col-2">{{ user.first_name }}</div>
-        <div class="col-2">{{ user.last_name }}</div>
-        <div class="col-3">{{ user.email }}</div>
-        <div class="col-2">
-          <button @click="removeUser(index)">delete</button>
-        </div>
-        <div class="col-2"><button>edit</button></div>
-      </div>
+      <input
+        type="text"
+        id="myInput"
+        v-model="search_box"
+        placeholder="Search for names.."
+        title="Type in a name"
+      />
+
+      <table id="myTable" class="table text-white text-center">
+        <thead>
+          <tr>
+            <th scope="col">avtar</th>
+            <th scope="col">first name</th>
+            <th scope="col">last name</th>
+            <th scope="col">email</th>
+            <th colspan="2">edit / delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(user, index) in users" :key="user.id">
+            <td><img class="w-25" :src="user.avatar" alt="" /></td>
+            <td>{{ user.first_name }}</td>
+            <td>{{ user.last_name }}</td>
+            <td>{{ user.email }}</td>
+            <td>
+              <button
+                v-b-modal.modal-2
+                @click="
+                  editUserForm(
+                    user.avatar,
+                    user.first_name,
+                    user.last_name,
+                    user.email,
+                    index
+                  )
+                "
+              >
+                edit
+              </button>
+              <button @click="removeUser(index)">delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <button class="btn btn-success" v-b-modal.modal-1>add user</button>
     </div>
+    <div>
+      <b-modal hide-footer ref="modal-1" id="modal-1">
+        <div class="text-dark">
+          <h1 class="text-center">add user</h1>
+          <div class="form-group">
+            <label for="exampleInputEmail1">first name</label>
+            <input type="text" class="form-control" v-model="new_name" />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Last name</label>
+            <input type="text" class="form-control" v-model="new_last_name" />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" v-model="new_email" />
+          </div>
 
-    <form>
-      <div class="form-group">
-        <label for="exampleInputEmail1">first name</label>
-        <input type="text" class="form-control" v-model="new_name" />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Last name</label>
-        <input type="text" class="form-control" v-model="new_last_name" />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" v-model="new_email" />
-      </div>
+          <button
+            :disabled="new_name == '' || new_email == '' || new_last_name == ''"
+            type="submit"
+            class="btn btn-primary"
+            @click="adduser(), hideModalAddUsers()"
+          >
+            Add New User
+          </button>
+        </div>
+      </b-modal>
+    </div>
+    <div>
+      <b-modal hide-footer ref="modal-2" id="modal-2">
+        <div class="text-dark">
+          <h1 class="text-center">edit user</h1>
+          <div class="form-group">
+            <label for="exampleInputEmail1">avatar </label>
+            <input type="text" class="form-control" v-model="edited_avatar" />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">first name</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="edited_first_name"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Last name</label>
+            <input
+              type="text"
+              class="form-control"
+              v-model="edited_last_name"
+            />
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input type="email" class="form-control" v-model="edited_email" />
+          </div>
 
-      <button
-        :disabled="new_name == '' || new_email == '' || new_last_name == ''"
-        type="submit"
-        class="btn btn-primary"
-        @click="adduser()"
-      >
-        Submit
-      </button>
-    </form>
-    {{ users }}
+          <button
+            :disabled="
+              edited_avatar == '' ||
+              edited_first_name == '' ||
+              edited_last_name == '' ||
+              edited_email == ''
+            "
+            class="btn btn-primary"
+            @click="editUser(), hideModaleditUsers()"
+          >
+            Save changes
+          </button>
+        </div>
+      </b-modal>
+    </div>
   </div>
 </template>
 
 <script>
+// vue.filter("filterSearch", function () {
+//   return this.users.filter((user) => {
+//     return user.first_name.match(this.search_box);
+//   });
+// });
 export default {
   data() {
     return {
@@ -49,15 +137,31 @@ export default {
       new_email: "",
       remove_user_id: 0,
       edited_user_id: 0,
-      edited_name: "",
+      edited_first_name: "",
       edited_last_name: "",
       edited_email: "",
+      edited_avatar: "",
+      edited_index: 0,
+      search_box: "",
     };
   },
-  mounted() {
+  created() {
     this.listOfUsers();
   },
+  computed: {
+    // filterSearch: function () {
+    //   return this.users.filter((user) => {
+    //     return user.first_name.match(this.search_box);
+    //   });
+    // },
+  },
   methods: {
+    hideModalAddUsers() {
+      this.$refs["modal-1"].hide();
+    },
+    hideModaleditUsers() {
+      this.$refs["modal-2"].hide();
+    },
     listOfUsers() {
       var self = this;
       this.$axios
@@ -82,13 +186,32 @@ export default {
 
       //   new_user.push('"email"' + ":" + this.new_email);
       new_user.email = this.new_email;
-
       this.users.push(new_user);
+      this.new_name = "";
+      this.new_last_name = "";
+      this.new_email = "";
     },
     removeUser(indexOf) {
       this.users.splice(indexOf, 1);
     },
-    editUser() {},
+    editUserForm(avatar, first_name, last_name, email, index) {
+      this.edited_avatar = avatar;
+      this.edited_first_name = first_name;
+      this.edited_last_name = last_name;
+      this.edited_email = email;
+      this.edited_index = index;
+    },
+
+    editUser() {
+      this.users[this.edited_index].first_name = this.edited_first_name;
+      this.users[this.edited_index].last_name = this.edited_last_name;
+      this.users[this.edited_index].email = this.edited_email;
+      this.users[this.edited_index].avatar = this.edited_avatar;
+      this.edited_first_name = "";
+      this.edited_last_name = "";
+      this.edited_email = "";
+      this.edited_avatar = "";
+    },
   },
 };
 </script>
